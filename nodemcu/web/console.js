7712e79c21d1c25d2ff4fc9ca0ddb1c558d88b4c
Captain-IOT <71851836+Captain-IOT@users.noreply.github.com>
2020-10-05 12:27:23 -0500

    Update console.js

--- nodemcu/web/console.js
+++ nodemcu/web/console.js
@@ -1,3 +1,66 @@
+function setControl(baseUri,id){
+	var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+	    if (this.readyState==4 && this.status==200){
+			var text = this.responseText;
+
+			var widget = document.getElementById(id);
+			if (widget instanceof HTMLButtonElement){
+				if (text=='1')
+					widget.className='buttonTrue';
+				else
+					widget.className='buttonFalse';
+			}
+			else if (widget instanceof HTMLInputElement){
+				widget.value=text;
+			}
+			else if (widget instanceof HTMLSelectElement){
+				widget.value=text;
+			}
+		}
+	}
+  	xhttp.open("GET",baseUri+"/property?key="+id,true);
+  	xhttp.send();	
+}
+
+function sendUIUpdate(uri,id){
+	var value = "?value=";
+	var widget = document.getElementById(id);
+	if (widget instanceof HTMLButtonElement){
+		value += widget.className.contains('buttonTrue') ? "0" : "1";
+	}
+	else if (widget instanceof HTMLInputElement){
+		value += widget.value;
+	}
+	else if (widget instanceof HTMLSelectElement){
+		value += widget.value;
+	}
+	var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+	    if (this.readyState==4 && this.status==200){
+			var text = this.responseText;
+
+			var widget = document.getElementById(id);
+			if (widget instanceof HTMLButtonElement){
+				if (text=='1')
+					widget.className='buttonTrue';
+				else
+					widget.className='buttonFalse';
+			}
+			else if (widget instanceof HTMLInputElement){
+				widget.value=text;
+			}
+			else if (widget instanceof HTMLSelectElement){
+				widget.value=text;
+			}
+		}
+	}
+  	xhttp.open("GET",uri+value,true);
+  	xhttp.send();	
+}
+	
+	
+
 function req(uri,id){
 	var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function(){
